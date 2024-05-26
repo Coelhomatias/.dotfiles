3c27cad4cfde73cd72554a2a61649c3baf716e58
Coelhomatias <leandrohenrique.lf@gmail.com>
2024-05-26 11:55:44 +0100

    fixed some issues with the bar

--- .config/ags/widget/bar/Bar.ts
+++ .config/ags/widget/bar/Bar.ts
@@ -5,6 +5,7 @@ import Date from "./buttons/Date"
 const Bar = (monitor: number) => Widget.Window({
     monitor,
     name: `bar-${monitor}`,
+    exclusivity: "exclusive",
     anchor: ["top", "left", "right"],
     child: Widget.CenterBox({
         endWidget: Widget.Box({

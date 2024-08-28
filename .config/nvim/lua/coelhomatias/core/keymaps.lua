0070803f59a6a209542c07a426226ac5cd8762fd
Coelhomatias <lenadrohenrique.lf@gmail.com>
2024-08-28 17:47:43 +0100

    removed ., keymap, going to try Esc

--- .config/nvim/lua/coelhomatias/core/keymaps.lua
+++ .config/nvim/lua/coelhomatias/core/keymaps.lua
@@ -2,7 +2,7 @@ vim.g.mapleader = " "
 
 local keymap  = vim.keymap -- for conciseness
 
-keymap.set("i", ".,", "<ESC>", { desc = "Exit insert mode with .," })
+-- keymap.set("i", ".,", "<ESC>", { desc = "Exit insert mode with .," })
 
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights"})
 

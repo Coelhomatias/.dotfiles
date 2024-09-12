6f0fdc25458f97ecdf988dfb65b46fe12c8fb816
Coelhomatias <leandrohenrique.lf@gmail.com>
2024-09-12 21:49:37 +0100

    added autocompletions and changed some configs

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -4,7 +4,15 @@ return {
   config = function()
     require("catppuccin").setup({
       flavour = "mocha",
-      transparent_background = true
+      transparent_background = true,
+      integrations = {
+        indent_blankline = {
+          enabled = true,
+          scope_color = "mauve", -- catppuccin color (eg. `lavender`) Default: text
+          colored_indent_levels = false,
+        },
+        -- harpoon = true
+      }
     })
     vim.cmd.colorscheme "catppuccin"
   end
